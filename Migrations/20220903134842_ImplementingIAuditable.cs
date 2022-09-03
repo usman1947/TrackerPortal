e168a49a9f319bcab4fb66c70c633cc0bc40f093
Usman <usman.aslam@yicom.com>
2022-09-03 21:50:17 +0800

    added migration for auditable fields

---
+++ Migrations/20220903134842_ImplementingIAuditable.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class ImplementingIAuditable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "WorkoutProgramUserMapping",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "WorkoutProgramMapping",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "WorkoutProgram",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "WorkoutExerciseMapping",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Workout",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Set",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Exercise",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Exercise",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "WorkoutProgramUserMapping");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "WorkoutProgramMapping");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "WorkoutProgram");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "WorkoutExerciseMapping");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Workout");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Set");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Exercise");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Exercise");
+        }
+    }
+}
